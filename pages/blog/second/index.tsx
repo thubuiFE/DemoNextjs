1c34d2fed593891d7217d99380d164cfc560559e
thubuiFE <thu.bui@executionlab.asia>
2022-11-16 16:37:46 +0700

    fix error prettier

--- pages/blog/second/index.tsx
+++ pages/blog/second/index.tsx
@@ -1,17 +1,17 @@
-
 import React from "react";
 
 interface Props {
-    text: string,
-    handleClick: () => void;
+  text: string;
+  handleClick: () => void;
 }
 
 const BlogSecond = ({ handleClick, text }: Props) => (
-    <div style={{textAlign: "center"}}>
-        <button type="button" onClick={handleClick}>{text}</button>
-        <p>{text}</p>
-    </div>
-    
-); 
+  <div style={{ textAlign: "center" }}>
+    <button type="button" onClick={handleClick}>
+      {text}
+    </button>
+    <p>{text}</p>
+  </div>
+);
 
 export default BlogSecond;
